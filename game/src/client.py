51bd67321f9e46572ea439397541da7deb358d6c
fashen <2085605952@qq.com>
2021-10-03 15:54:17 +0800

    add match_client

---
+++ game/src/client.py
@@ -0,0 +1,33 @@
+from match_client.match import Match
+from match_client.match.ttypes import User
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+
+def main():
+    # Make socket
+    transport = TSocket.TSocket('localhost', 9090)
+
+    # Buffering is critical. Raw sockets are very slow
+    transport = TTransport.TBufferedTransport(transport)
+
+    # Wrap in a protocol
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+    # Create a client to use the protocol encoder
+    client = Match.Client(protocol)
+
+    # Connect!
+    transport.open()
+
+    user = User(1, 'lyw', 1500)
+    client.add_user(user, "")
+
+    # Close!
+    transport.close()
+
+if __name__ == "__main__":
+    main()
